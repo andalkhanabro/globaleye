72e639c2eabcbcc63c8eda2d3f4028646557f7da
Andal Abro <aandalkhan16@gmail.com>
2025-04-10 19:20:27 -0700

    added basic OCR functionality (pytesseract)

--- src/imports.py
+++ src/imports.py
@@ -3,3 +3,5 @@ import pandas as pd
 import numpy as np 
 import pytesseract as tess 
 import cv2 
+import langdetect 
+import requests 
--- src/ocr.py
+++ src/ocr.py
@@ -1,18 +1,38 @@
+#! /usr/bin/env python3
+
 from imports import *
 
-def image_to_text(imagepath):
+
+SUPPORTED_LANGS = [
+    'eng',  
+    'fra',  
+    'deu', 
+    'spa', 
+    'ita',  
+    'por',  
+    'rus',
+    'ara', 
+    'hin',
+    'jpn',  
+    'chi_sim',
+    'kor', 
+    'tur', 
+    'heb',  
+    'ben',  
+    'tam',  
+    'urd',  
+    'vie',  
+]
+
+LANG_STRING = "+".join(SUPPORTED_LANGS)
+
+def image_to_text(imagepath, user_lang=None):
     imagepath = imagepath
-    return tess.image_to_string(imagepath,lang='deu')
 
+    if user_lang is None:
+        return tess.image_to_string(imagepath, lang = LANG_STRING) # adding too many langs can confuse extraction...
+    else: 
+        return tess.image_to_string(imagepath, lang = user_lang) # TODO: probably check if user inputted lang is a supported lang or ALLOW from a supported menu 
 
-print(tess.get_languages(config=''))
 test_text = image_to_text("testimages/german2.jpeg")
-
-# idea: image comes in, OCR is done
-# lang is detected after 
-
-
-
-
-
-
+print(f"Raw Text: {test_text}")
